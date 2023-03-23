d5175ae4158afa8ec3e6609551cde6eb0e5a9471
Ryan Borra <59072993+borraborra@users.noreply.github.com>
2023-03-23 14:07:40 -0400

    Add files via upload

---
+++ c31.py
@@ -0,0 +1,18 @@
+xy=input('Enter Hours:')
+abc = int(xy)
+xyz=input('Enter Rate:')
+abcd = int(xyz)
+if abc > 40:
+    fty = abc-40
+    ftyd = fty *1.5 *abcd
+    ab = ftyd + 40 *abcd
+    print(ab)
+elif abc < 40:
+    ghi = abc*abcd
+    print(ghi)
+#ab = abc*abcd
+
+
+#ab = abc*1.8 + 32
+#print('Based on the celsius temperature, the current fahrenheit temp is',ab)
+#print('Have a nice day!')
